09014da577de24de1633ac0f2beac461c391231c
Carlos Andres <carlandre6022@gmail.com>
2024-04-20 06:44:57 -0500

    Completando la clase 1

---
+++ Curso de Fundamentos de Python/1. Por qué aprender a programar en Python.py
@@ -0,0 +1,6 @@
+#Clase 1: ¿Por qué aprender a programar en Python?
+'''
+1. Está en el top 20 de los lenguajes de programación mejor pagos.
+2. Puede ser utilizado en diferentes áreas, especialmente en Análisis de datos (51%) y desarrollo web (45%).
+3. Tiene una demanda laboral muy grande en el mundo de la tecnología.
+'''
